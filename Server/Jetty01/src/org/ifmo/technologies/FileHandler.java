164796707a8aff5bccdceb10d71026317b31696f
Alexander Kubenskiy <akoubensky@gmail.com>
2019-08-12 17:40:41 +0300

    Parameterization

--- Server/Jetty01/src/org/ifmo/technologies/FileHandler.java
+++ Server/Jetty01/src/org/ifmo/technologies/FileHandler.java
@@ -1,9 +1,11 @@
 package org.ifmo.technologies;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.util.Properties;
 
 /**
@@ -12,19 +14,26 @@ import java.util.Properties;
  * если, конечно, такой файл (страница) существует.
  */
 public class FileHandler {
-
+    final static String propsName = "server/jetty01/param.properties";
 
 	public static void main(String[] args) {
         Server server = new Server(8080);
 		Properties props = new Properties();
+		String handlerName;
+		String root;
 		try {
-			props.load(new FileReader("e:/teaching/sergey/project/server/jetty01/param.properties"));
+			props.load(new FileReader(propsName));
+			handlerName = props.getProperty("handler");
+			root = props.getProperty("root");
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
-		server.setHandler(new MyHandler(props.getProperty("root")));
         try {
+            Class<Handler> handlerClass = (Class<Handler>)Class.forName(handlerName);
+            Constructor<Handler> handlerConstructor = handlerClass.getConstructor(String.class);
+            Handler handler = handlerConstructor.newInstance(root);
+            server.setHandler(handler);
 			server.start();
 		} catch (Exception e) {
 			e.printStackTrace();
